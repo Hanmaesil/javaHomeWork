47e81641485515b085340e9e299828c51d94a4f7
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-11-27 19:42:05 +0900

    2021.11.26

---
+++ Day09.java
@@ -0,0 +1,75 @@
+package homework;
+
+import java.util.Scanner;
+
+public class Day09 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		// 1번 : 정수 N을 입력받아 N * N 배열에 다음과 같이 숫자를 저장하고 출력하시오.
+//		System.out.print("정수 N 입력 : ");
+//		int num = sc.nextInt();
+//		int[][] ary = new int[num][num];
+//		
+//		for(int i = 0; i < ary.length; i++) {
+//			for(int j = 0; j < ary.length; j++) {
+//				ary[i][j] = (j * 5) + (i + 1);
+//			}
+//		}
+//		for(int i = 0; i < ary.length; i++) {
+//			for(int j = 0; j < ary.length; j++) {
+//				System.out.print(ary[i][j] + " ");
+//			}System.out.println();
+//		}
+
+		// 2번 : 아래와 같은 2차원 배열을 왼쪽으로 180도 회전하여 출력하시오.
+//		System.out.println("원본");
+//
+//		int[][] ary = new int[5][5];
+//		int num = 1;
+//		for (int i = 0; i < ary.length; i++) {
+//			for (int j = 0; j < ary.length; j++) {
+//				ary[i][j] = num;
+//				num += 1;
+//
+//			}
+//		}
+//		for (int i = 0; i < ary.length; i++) {
+//			for (int j = 0; j < ary.length; j++) {
+//				System.out.printf("%3d", ary[i][j]);
+//			}
+//			System.out.println();
+//
+//		}
+//		
+//		System.out.println("180도 회전");
+//		for (int i = 4; i >= 0; i--) {
+//			for (int j = 4; j >= 0; j--) {
+//				System.out.printf("%3d", ary[i][j]);
+//			}
+//			System.out.println();
+//
+//	}
+		//3번 : 2차원 배열에 아래와 같이 다이아몬드형태로 출력하시오.
+		//int[][] ary = new int[7][7];
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}
+}
